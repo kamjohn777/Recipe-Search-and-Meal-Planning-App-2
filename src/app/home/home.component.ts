ef1264e01e382b2f4c4b5fe47f7138304eb91d20
kamjohn777 <kamjohn79.kdj@gmail.com>
2023-09-08 11:19:06 -0500

    updated home page and header

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+// import { RecipeService } from './path-to-recipe-service'; 
+// import { Router } from '@angular/router';
+// import { FavoriteService } from './path-to-favorite-service'; 
 
 @Component({
   selector: 'app-home',
@@ -6,5 +9,71 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  // recipes: any[] = [];
+  // searchTerm: any = '';
+  // categories: any[] = [];
+  // selectedCategory :string ='';
+  // selectedRecipeId: any = '';
+  // filteredRecipes: any[] = [];
+  // displayedRecipes: any[] = [];
+  // pageSize: number = 10;
+  // favoriteRecipes: any[] = [];
 
+  // constructor(private recipeService: RecipeService, private router: Router, private favoriteRecipeService: FavoriteService) {}
+  // fetchRecipesByCategory(): void {
+  //   this.recipeService.getRecipesByCategory(this.selectedCategory).subscribe((data: any) => {
+  //     this.recipes = data.meals;
+  //   });
+  // }
+
+  // constructor(
+  //   private recipeService: RecipeService,
+  //   private router: Router,
+  //   private favoriteRecipeService: FavoriteService
+  // ) {}
+
+  // fetchCategories(): void {
+  //   this.recipeService.getMealCategories().subscribe((data: any) => {
+  //     this.categories = data.categories;
+  //   });
+  // }
+
+  // fetchRecipesByCriteria(criteria: string): void {
+  //   this.recipeService.getRecipesBySearch(criteria).subscribe((data: any) => {
+  //     this.recipes = data.meals;
+  //   });
+  // }
+
+  // onFavoriteClick(recipe: any): void {
+  //   this.favoriteRecipeService.addFavoriteRecipe(recipe);
+  // }
+
+  // isRecipeInFavorites(recipeId: string): boolean {
+  //   return this.favoriteRecipeService.isRecipeInFavorites(recipeId);
+  // }
+  
+  // openRecipeDetails(recipeId: string): void {
+  //   this.router.navigate(['/meal', recipeId]);
+  // }
+  
+  // onRecipeFavorite(recipe: any) {
+  //   this.favoriteRecipeService.addFavoriteRecipe(recipe);
+    
+  // }
+
+  // fetchRecipesBySearchTerm(): void {
+  //   this.recipeService.getRecipesBySearch(this.searchTerm).subscribe((data: any) => {
+  //     console.log(data)
+  //     this.recipes = data.meals;
+  //   });
+  // }
+
+  // performSearch() {
+  //   this.fetchRecipesBySearchTerm();
+  // }
+  
+  // ngOnInit(): void {
+  //   this.fetchCategories();
+  //   this.fetchRecipesBySearchTerm();
+  // }
 }
